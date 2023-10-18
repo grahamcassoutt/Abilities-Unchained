15333e961eaa945d68ea376115b2b535c62c87df
Jeremy Marchesani <jeremy12@iastate.edu>
2023-10-18 18:40:58 -0500

    Initial Commit

---
+++ Abilities-Unchained-Card-Duels/Abilities_Unchained_Card_DuelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Abilities_Unchained_Card_DuelsApp.swift
+//  Abilities-Unchained-Card-Duels
+//
+//  Created by Jeremy Marchesani on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Abilities_Unchained_Card_DuelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
